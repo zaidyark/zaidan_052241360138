f7b1e1745ceff46d8a4550c6546a0e3b04ea4f0b
Yakubu Zaidan <zaidyark@gmail.com>
2024-03-03 07:05:06 +0000

    First Faculty Websit Design

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Faculty Website</title>
+    <link rel="stylesheet" href="Styles.css">
+</head>
+<style>
+    body {
+        margin: 0;
+        padding: 0;
+    }
+    img {
+        width: 100%;
+        height: 100%;
+
+    }
+</style>
+</head>
+<header>
+        <h1>Welcome to Faculty Of Applied Sciences</h1>
+        <nav>
+            <ul>
+                <li><a href="department.html">Departments</a></li>
+                <li><a href="faculty.html">Faculties</a></li>
+                <li><a href="timetable.html">Timetables</a></li>
+                <li><a href="payment.html">Payment</a></li>
+                <li><a href="login.html">Login</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+
+    </main>
+<body>
+</body>
+    <img src="Kstu.jpg" alt="" width="50" height="50">
+</body>
+
+</html>
---
+++ menu.html
@@ -0,0 +1,9 @@
+<!-- menu.html -->
+<nav>
+	<ul>
+		<li><a href="faculty.php"> Faculty</a></li>
+		<li><a href="departments.php">Departments</a></li>
+		<li><a href="timetables.php">Timetables</a></li>
+		<li><a href="payments.php">Payments</a></li>
+	</ul>
+</nav>
